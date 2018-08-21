8df5c61133e7e69cd9bd424c07b76122b6594c5f
Chris McClaskey <mcclaskc@users.noreply.github.com>
2018-08-21 13:19:11 -0600

    Create wtf.ts

---
+++ wtf.ts
@@ -0,0 +1,42 @@
+type MyObject = {
+    [index:string]: MyObject | Function | "wtf"
+}
+
+let myObj: MyObject;
+
+// compiles
+myObj = {
+    name: "wtf",
+};
+
+// compiles - note the nested key is `notName:`, instead of `name:`
+myObj = {
+    wrapper: {
+        notName: "wtf",
+    },
+};
+
+// as soon as I change the nested key to `name:`, it fails to compile:
+/*
+
+Type '{ name: string; }' is not assignable to type 'Function | MyObject | "wtf"'.
+  Type '{ name: string; }' is not assignable to type '"wtf"'
+ 
+*/
+myObj = {
+    wrapper: {
+        name: "wtf",
+    },
+};
+
+
+// Other weirdness, if I remove `Function` from the union, it compiles:
+type MyObjectNoFunction = {
+    [index:string]: MyObjectNoFunction | "wtf"
+}
+
+let myObjNoFn: MyObjectNoFunction = {
+    wrapper: {
+        name: "wtf"
+    }
+};
